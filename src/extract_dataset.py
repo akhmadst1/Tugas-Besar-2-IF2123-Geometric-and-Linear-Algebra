165805c3ef956a7f0edce0c34336ffe15d162d60
blixa-rd <16521074@mahasiswa.itb.ac.id>
2022-11-06 01:30:11 +0700

    Cleanup + Adding Dataset

--- src/extract_dataset.py
+++ src/extract_dataset.py
@@ -2,18 +2,19 @@ import cv2
 import os
 import numpy
 
-folder = input("Masukkan nama folder: ")
-images = []
-sum_images = [[0 for j in range(256)] for i in range(256)]
-count = 0
-for filename in os.listdir(folder):
-    img = cv2.imread(os.path.join(folder, filename), cv2.IMREAD_GRAYSCALE)
+folder = input("Masukkan nama folder dataset: ") # contoh input: dataset_taylor
+images = []  # memori untuk nyimpen tiap foto yang ada di folder ke array images, udah dikonversi ke matriks
+sum_images = [[0 for j in range(256)] for i in range(256)]  # matriks untuk nyimpen hasil penjumlahan semua foto, dipake 256x256
+count = 0  # jumlah foto yang valid
+
+for filename in os.listdir(r"..\Algeo02-21115\test\{}".format(folder)):
+    img = cv2.imread(os.path.join(r"..\Algeo02-21115\test\{}".format(folder), filename), cv2.IMREAD_GRAYSCALE)
     if img is not None:
-        img = cv2.resize(img, (256, 256))
-        images.append(img)
-        sum_images = numpy.add(sum_images, img)
+        img = cv2.resize(img, (256, 256))  # resize foto ke ukuran 256x256
+        images.append(img)  # tiap foto disimpen ke array images
+        sum_images = numpy.add(sum_images, img)  # ngejumlahin tiap pixel semua foto
         count += 1
 
-mean = numpy.divide(sum_images, count)
-print(mean)
-cv2.imwrite("test\contoh.jpg", mean)
+mean = numpy.divide(sum_images, count) # jumlah matriks image dibagi jumlah fotonya
+cv2.imwrite(r"..\Algeo02-21115\test\{}\mean_image.jpg".format(folder), mean) # image hasil rata-rata disimpen ke mean_image.jpg
+# print(mean)
--- src/extract_input.py
+++ src/extract_input.py
@@ -2,3 +2,11 @@ import cv2
 import os
 import numpy
 
+# untuk ekstraksi foto input yang mau dicocokkan sama dataset
+
+images = []
+filename = input("Masukkan nama file tanpa format .jpg : ") # contoh input: taylor
+img = cv2.imread(r"..\Algeo02-21115\test\{}.jpg".format(filename), cv2.IMREAD_GRAYSCALE)
+images.append(img)
+
+cv2.imwrite(r"..\Algeo02-21115\test\{}_extracted.jpg".format(filename), img) # menghasilkan foto hasil ekstraksi
