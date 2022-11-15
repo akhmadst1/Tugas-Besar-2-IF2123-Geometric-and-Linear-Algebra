a5438925a7d2d7867b33509be5a563849892bc64
asyifashfr <16521132@mahasiswa.itb.ac.id>
2022-11-15 09:34:19 +0700

    selisih training image dgn nilai tengah

--- src/selisih.py
+++ src/selisih.py
@@ -1,16 +1,8 @@
 from extract_dataset import sum_images
 from extract_dataset import mean
 
-matrixSelisih = []
-
 for i in range(0, len(sum_images)):
-    res = 0
-    row =[]
     for j in range(0, len(sum_images[0])):
-        res = sum_images[i][j] - mean
-        row.append(res)            
-    print(row)
-    matrixSelisih.append(row)
-
-print(matrixSelisih)
+        matrixSelisih = sum_images[i][j] - mean
+    print(matrixSelisih, end=' ')
 
