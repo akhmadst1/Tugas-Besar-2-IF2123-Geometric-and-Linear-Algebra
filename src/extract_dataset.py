5a00da48a3e9cd37eca728b012e4a52a89c937fa
blixa-rd <16521074@mahasiswa.itb.ac.id>
2022-11-16 14:39:14 +0700

    [ADD] step ekstraksi - kovarian

--- src/extract_dataset.py
+++ src/extract_dataset.py
@@ -2,20 +2,37 @@ import cv2
 import os
 import numpy
 
+# 1. EKSTRAKSI DATASET
 folder = input("Masukkan nama folder dataset: ") # contoh input: dataset_taylor
 images = []  # memori untuk nyimpen tiap foto yang ada di folder ke array images, udah dikonversi ke matriks
-sum_images = [[0 for j in range(256)] for i in range(256)]  # matriks untuk nyimpen hasil penjumlahan semua foto, dipake 256x256
 count = 0  # jumlah foto yang valid
 
 for filename in os.listdir(r"..\Algeo02-21115\test\{}".format(folder)):
     img = cv2.imread(os.path.join(r"..\Algeo02-21115\test\{}".format(folder), filename), cv2.IMREAD_GRAYSCALE)
     if img is not None:
         img = cv2.resize(img, (256, 256))  # resize foto ke ukuran 256x256
-        images.append(img)  # tiap foto disimpen ke array images
-        sum_images = numpy.add(sum_images, img)  # ngejumlahin tiap pixel semua foto
+        img = img.flatten() # ubah dimensi matriks foto jadi 1 dimensi: 256^2 x 1
+        images.append(img)  # tiap training set disimpen ke array images
         count += 1
 
-mean = numpy.divide(sum_images, count) # jumlah matriks image dibagi jumlah fotonya
-cv2.imwrite(r"..\Algeo02-21115\test\{}\mean_image.jpg".format(folder), mean)    # image hasil rata-rata disimpen ke mean_image.jpg
-                                                                                # di folder test/dataset_namaorangnya (cth: dataset_taylor)
-print(mean) # ngetes print nilai matriks foto yang udah dirata-ratain
+# 2. MENCARI NILAI RATAAN DATASET
+sum_images = [0 for j in range(65536)]  # array penyimpan jumlah semua nilai matrix images: (256^2 = 65536)
+for i in range(count):
+    sum_images = numpy.add(sum_images, images[i])  # ngejumlahin tiap pixel semua matrix foto
+mean = numpy.divide(sum_images, count)
+
+# 3. MENCARI SELISIH TRAINING SET DENGAN RATAAN
+for i in range(count):
+    images[i] = numpy.subtract(images[i], mean)
+
+# 4. MENCARI KOVARIAN
+# kovarian ukuran m x m (m jumlah training set)
+covariant = numpy.dot(images, numpy.transpose(images))
+# print(covariant)
+cv2.imwrite(r"..\Algeo02-21115\test\covariant.jpg", covariant)
+
+# (!!!!! JANGAN DICOBAA !!!!!)
+# kovarian ukuran 65536 x 65536
+# covariant2 = numpy.dot(numpy.transpose(images), images)
+# # print(covariant2)
+# cv2.imwrite(r"..\Algeo02-21115\test\covariant2.jpg", covariant2)
