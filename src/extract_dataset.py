04787ec2ee5874a137d30eefe19802e2e7b50ffc
blixa-rd <16521074@mahasiswa.itb.ac.id>
2022-11-21 19:47:01 +0700

    [ADD] GUI Test

--- src/extract_dataset.py
+++ src/extract_dataset.py
@@ -3,95 +3,157 @@ import os
 import numpy
 import numpy.linalg as lin
 from eigenvalues import *
+import time
+
 #EKSTRASI DATA SET 
-def ekstrasiDataSet(folder):
-    images = []  # memori untuk nyimpen tiap foto yang ada di folder ke array images, udah dikonversi ke matriks
+def ekstrasiDataSet(path_folder):
+    images_clr = []  # image berwarna
+    images_bnw = []  # memori untuk nyimpen tiap foto yang ada di folder ke array images, udah dikonversi ke matriks grayscale
     count = 0  # jumlah foto yang valid
 
-    for filename in os.listdir(r"..\Algeo02-21115\test\{}".format(folder)):
-        img = cv2.imread(os.path.join(r"..\Algeo02-21115\test\{}".format(folder), filename), cv2.IMREAD_GRAYSCALE)
-        if img is not None:
-            img = cv2.resize(img, (256, 256))  # resize foto ke ukuran 256x256
-            img = img.flatten() # ubah dimensi matriks foto jadi 1 dimensi: 256^2 x 1
-            images.append(img)  # tiap training set disimpen ke array images
+    for filename in os.listdir(path_folder):
+        img1 = cv2.imread(os.path.join(path_folder, filename), cv2.IMREAD_GRAYSCALE)
+        img2 = cv2.imread(os.path.join(path_folder, filename), cv2.IMREAD_UNCHANGED)
+        width = 256
+        height = 256
+        dim = (width, height)
+        if img1 is not None:
+            img1 = cv2.resize(img1, (256, 256))  # resize foto ke ukuran 256x256
+            img1 = img1.flatten() # ubah dimensi matriks foto jadi 1 dimensi: 256^2 x 1
+            images_bnw.append(img1)  # tiap training set disimpen ke array images
             count += 1
-    newimages = numpy.vstack(images)
-    newimages = transpose(images)
-    return images,count ,newimages                               #outputnya images gabungan hasil flatten extract foto 
+        if img2 is not None:
+            # resize image
+            resized = cv2.resize(img2, dim, interpolation = cv2.INTER_AREA)
+            # img2 = cv2.resize(img2, (256, 256))  # resize foto ke ukuran 256x256
+            # img2 = img2.flatten() # ubah dimensi matriks foto jadi 1 dimensi: 256^2 x 1
+            images_clr.append(resized)  # tiap training set disimpen ke array images
+    # newimages_clr = numpy.vstack(images_clr)
+    # newimages_clr = transpose(images_clr)
+    newimages = numpy.vstack(images_bnw)
+    newimages = transpose(images_bnw)
+    return images_bnw, count, newimages, images_clr                             #outputnya images gabungan hasil flatten extract foto 
+
+def extract_test_input(path_input):
+    image = []
+    # filename = input("Masukkan nama file tanpa format .jpg : ") # contoh input: taylor
+    img = cv2.imread(path_input, cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (256, 256))
+    img = img.flatten()
+    image.append(img)
+    newimagetest = numpy.vstack(image)
+    newimagetest = transpose(image)
+    return newimagetest
+
+def get_test_image(path_input):
+    img = cv2.imread(path_input, cv2.IMREAD_UNCHANGED)
+    width = 256
+    height = 256
+    dim = (width, height)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    cv2.imwrite(r"..\Algeo02-21115\src\assets\training.png", resized)
 
 def rataRata(matrix):
-    mean = newimages.mean(1)
+    mean = matrix.mean(1)
     return mean
-def selisihMatrix(newimages,meanface):
+
+def selisihMatrix(newimages, meanface):
     size = newimages.shape[1]
     newfaces = numpy.zeros(newimages.shape)
-    for i in range(0,size):
-        newfaces[:,i] = np.subtract(newimages[:,i],meanface)
+    for i in range(0, size):
+        newfaces[:, i] = np.subtract(newimages[:, i], meanface)
     return newfaces
+
 def covariant(newfaces):
-    L = numpy.dot(numpy.transpose(newfaces),newfaces)
+    L = numpy.dot(numpy.transpose(newfaces), newfaces)
     return L 
+
 def eigenvectors(L):
-    eigenvalues,eigenvectors = HessenbergQR(L) 
+    eigenvalues, eigenvectors = HessenbergQR(L) 
     return eigenvectors
+
 def eigenface(newfaces,eigenvectors):
     u = numpy.dot(newfaces, eigenvectors)
     return u 
+
 def weight(u,newfaces):
     ut = numpy.transpose(u)
     w = numpy.dot(ut, newfaces)
-    return ut , w
-#proses training images 
-images,count,newimages = ekstrasiDataSet("dataset_taylor")
-mean = rataRata(newimages)
-size = (256,256)
-meanface = mean.reshape(size)
-cv2.imwrite(r"..\Algeo02-21115\test\mean\mean.jpg", meanface)
+    return ut, w
 
-#selisih 
-newfaces = selisihMatrix(newimages,mean)
-#covarian 
-L = covariant(newfaces)
-cv2.imwrite(r"..\Algeo02-21115\test\covariant\cov.jpg",L)
+def mainprogram(path_folder, path_input):
+    start = time.time()
 
-#eigenvalue dan eigenvektor 
-eigenvectors = eigenvectors(L)
+    # Proses training images 
+    images, count, newimages, newimages_clr = ekstrasiDataSet(path_folder)
+    mean = rataRata(newimages)
+    size = (256, 256)
+    meanface = mean.reshape(size)
+    # cv2.imwrite(r"..\Algeo02-21115\test\mean\mean.jpg", meanface)
 
-#Matrix u 
-u = numpy.dot(newfaces,eigenvectors)
-#write eigen face 
-size = (256,256)
-for i in range(0,u.shape[1]):
-    eigenface = u[:,5]
-    eigenfaces = eigenface.reshape(size)
-    cv2.imwrite(r"..\Algeo02-21115\test\eigenfaces\faces6.jpg",eigenfaces)
+    # Selisih training images dengan image rata-rata
+    newfaces = selisihMatrix(newimages, mean)
+    # Matriks kovarian 
+    L = covariant(newfaces)
+    # cv2.imwrite(r"..\Algeo02-21115\test\covariant\cov.jpg",L)
 
-#nyari weight 
-v,w = weight(u,newfaces)
+    # Eigenvalue dan Eigenvektor 
+    eigenvector = eigenvectors(L)
+    ev, eigenvector = HessenbergQR(L)
 
+    # Matrix u 
+    u = numpy.dot(newfaces, eigenvector)
+    # Write eigen face 
+    # size = (256, 256)
+    # for i in range(0, u.shape[1]):
+    #     eigenface = u[:, 5]
+    #     eigenfaces = eigenface.reshape(size)
+        # cv2.imwrite(r"..\Algeo02-21115\test\eigenfaces\faces{}.jpg".format(i), eigenfaces)
 
-############################################################# TRAINING IMAGES FOR DATA TEST #######################################################################
+    # Nyari weight 
+    v, w = weight(u, newfaces)
 
-imagestest,counttest,newimagestest = ekstrasiDataSet("dataset_test")
-#selisih 
-newfacestest = selisihMatrix(newimagestest,mean)
+    ############################################################# TRAINING IMAGES FOR DATA TEST #######################################################################
 
-ut,wtest = weight(u, newfacestest)
+    newimagestest = extract_test_input(path_input)
 
-############################################################ MENCARI JARAK #######################################################################
-wtest = numpy.transpose(wtest)
-w = numpy.transpose(w) 
-distance = []
-for i in range(0, wtest.shape[0]):
-    distance = [lin.norm(numpy.subtract(wtest[i],wdatabase)) for wdatabase in w]
-#mencari nilai paling kecil dan indeks paling kecil 
-nilaimin = numpy.min(distance)
-idx = numpy.where(distance==nilaimin)
-print(nilaimin)
-print(idx)
-hasil = newimages[:,idx]
-size = (256,256)
-hasil = hasil.reshape(size)
-cv2.imwrite(r"..\Algeo02-21115\test\result\result.jpg", hasil)
+    # Selisih 
+    newfacestest = selisihMatrix(newimagestest, mean)
 
-#Kalo mau nyoba yang stephen pake linalg aja nyari eigenvektornya soalnya lama banget kalo pake yang manual 
+    ut, wtest = weight(u, newfacestest)
+
+    ############################################################ MENCARI JARAK #######################################################################
+    wtest = numpy.transpose(wtest)
+    w = numpy.transpose(w) 
+    distance = []
+    for i in range(0, wtest.shape[0]):
+        distance = [lin.norm(numpy.subtract(wtest[i], wdatabase)) for wdatabase in w]
+
+    # Mencari nilai paling kecil dan indeks paling kecil 
+    nilaimin = numpy.min(distance)
+    idx = numpy.where(distance == nilaimin)
+
+    # Misal kayak database taylor tapi test nya stephen jadi gak cocok 
+    if (nilaimin > 1000000):
+        # print("Sorry Foto Tidak Ditemukan di Database")
+        # print(nilaimin)
+        # print((idx[0])[0])
+        hasil = None
+    else:
+        # print("Foto Ditemukan") 
+        # print(nilaimin)
+        # print((idx[0])[0])
+        hasil = newimages_clr[(idx[0])[0]]
+        # size = (256,256)
+        # hasil_bnw = hasil.reshape(size)
+        # cv2.imwrite(r"..\Algeo02-21115\test\result\result.jpg", hasil)
+        # hasil_clr = cv2.imread(os.path.join(path_folder, filename), cv2.IMREAD_GRAYSCALE)
+        cv2.imwrite(r"..\Algeo02-21115\src\assets\result.png", hasil)
+
+    end = time.time()
+    duration = (end-start)
+    return hasil, duration
+    # Kalo mau nyoba yang stephen pake linalg aja nyari eigenvektornya soalnya lama banget kalo pake yang manual
+    # return hasil
+
+# mainprogram(r"C:\Users\Lenovo\Documents\tube\Algeo02-21115\test\dataset_taylor", r"C:\Users\Lenovo\Documents\tube\Algeo02-21115\test\dataset_test\taylor.jpg")
